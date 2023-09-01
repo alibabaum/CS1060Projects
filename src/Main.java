21b376fb1aded148c4bf83095971cd93ef6093fa
alibabaum <143457879+alibabaum@users.noreply.github.com>
2023-09-01 17:48:58 -0600

    First try best try

--- src/Main.java
+++ src/Main.java
@@ -7,8 +7,10 @@ public class Main {
         System.out.printf("Hello and welcome!");
         System.out.printf("Hello and welcome!");
 
+        String message = "I have a message.";
+
         // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        for (int i = 1; i <= 9; i++) {
 
             // Press Shift+F9 to start debugging your code. We have set one breakpoint
             // for you, but you can always add more by pressing Ctrl+F8.
